44d7c345242b35dd11ee58dc285e2920f9f93bc4
WANEMEALIA <nwanemealia@gmail.com>
2025-12-15 12:04:39 +0800

    updATE

--- labs/lab02/exercise5/test/exercise5.py
+++ labs/lab02/exercise5/test/exercise5.py
@@ -1,16 +1,9 @@
-side1 = float(input("Enter side 1: "))
-side2 = float(input("Enter side 2: "))
-side3 = float(input("Enter side 3: "))
-
 def is_valid_triangle(side1, side2, side3):
-
-    if side1 + side2 > side3 :
-        return True
-    elif side1 + side3 > side2 :
-        return True
-    elif side2 + side3 > side1 :
+    if side1 + side2 > side3 and side1 + side3 > side2 and side2 + side3 > side1:
         return True
     else :
         return False
     
-print(is_valid_triangle)
+print(is_valid_triangle(3,2,1))
+print(is_valid_triangle(0,-2,3))
+print(is_valid_triangle(5,5,10))
