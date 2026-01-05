dd6ad524039ab924a8bfc23f9582ae8eff6627c8
WANEMEALIA <nwanemealia@gmail.com>
2026-01-05 12:00:13 +0800

    u

--- labs/lab03/exercise2/exercise2.py
+++ labs/lab03/exercise2/exercise2.py
@@ -1,5 +1,16 @@
 def find_station(stations, name):
-    pass
+    for i in range(len(stations)):
+        if stations[i] == name:
+            return i
+    return -1
 
+        
 def count_stops(stations, start, stop):
-    pass
+    start_point = find_station(stations, start)
+    end_point = find_station(stations, stop)
+    
+    if start_point == -1 or end_point == -1 :
+        return -1
+    
+    count = end_point - start_point
+    return count
