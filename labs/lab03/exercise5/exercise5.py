b1b5a958bfacfa4177dd03667ad973813cbf3aad
WANEMEALIA <nwanemealia@gmail.com>
2026-01-05 13:08:15 +0800

    u

--- labs/lab03/exercise5/exercise5.py
+++ labs/lab03/exercise5/exercise5.py
@@ -1,5 +1,16 @@
 def get_position(cars, car_number):
-    pass
+    for i in range (len(cars)):
+        if cars[i] == car_number:
+            return i
+    return -1
 
 def has_overtaken(before, after, car1, car2):
-    pass
+    car1_bef = get_position(before, car1)
+    car2_bef = get_position(before, car2)
+
+    car1_after = get_position(after, car1)
+    car2_after = get_position(after, car2)
+
+    if car1_bef > car2_bef and car1_after < car2_after:
+        return True
+    return False
