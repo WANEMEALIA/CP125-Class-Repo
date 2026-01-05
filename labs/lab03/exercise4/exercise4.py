5365812b6452a3214a7423cc277739c07880b317
WANEMEALIA <nwanemealia@gmail.com>
2026-01-05 12:59:47 +0800

    updat

--- labs/lab03/exercise4/exercise4.py
+++ labs/lab03/exercise4/exercise4.py
@@ -1,5 +1,5 @@
 def has_warming_trend(temps):
-    for i in range (len(temps) - 1):
-       if (temps[i] > temps[i+1]) and (temps[i] > temps[i-1]) :
+    for i in range (len(temps) - 2):
+       if (temps[i+1] > temps[i]) and (temps[i+2] > temps[i+1]) :
            return True
     return False
