7127edbf762fa0ab4667e54b9918694599ad238b
WANEMEALIA <nwanemealia@gmail.com>
2026-01-19 11:49:40 +0800

    4

--- labs/lab05/exercise4/exercise4.py
+++ labs/lab05/exercise4/exercise4.py
@@ -1,7 +1,7 @@
 import math
 def filter_query_times(times):
     """
-    Remove slow outliers (mean + std deviation) and return sorted times.
+    Remove slow outliers (mean + std deviation) and return sorted .
     """
     total = 0
     for i in times :
@@ -9,7 +9,7 @@ def filter_query_times(times):
     mean = total/len(times)
 
     variance_sum = 0
-    for i in times :
+    for i in times:
         variance_sum += (i - mean) ** 2
     variance = variance_sum/len(times)
 
@@ -18,16 +18,16 @@ def filter_query_times(times):
     limit = mean + stan_dev
 
     cleaned_times = []
-    for i in times :
+    for i in times:
         if i <= limit :
-            cleaned_times.append[i]
+            cleaned_times.append(i)
 
     cleaned_times.sort()
     return cleaned_times
 
 
 # Test
-query_times = [45, 52, 48, 180, 51, 47, 50, 12]
-result = filter_query_times(query_times)
-print(f"Filtered Times: {result}")  
+query_ = [45, 52, 48, 180, 51, 47, 50, 12]
+result = filter_query_times(times)
+print(f"Filtered : {result}")  
 # Expected: [12, 45, 47, 48, 50, 51, 52]
