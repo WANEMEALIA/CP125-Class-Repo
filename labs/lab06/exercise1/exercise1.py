3bc4a1086ba7968b62d84d9f7f1a836a94b6f9ec
WANEMEALIA <nwanemealia@gmail.com>
2026-01-26 11:57:23 +0800

    y

--- labs/lab06/exercise1/exercise1.py
+++ labs/lab06/exercise1/exercise1.py
@@ -1,18 +1,18 @@
-def get_legit_power_users(log_data, bot_ids, threshold):
+def filter_bots(log_data, bot_ids, threshold):
     user_actions = {}
 
     for log_data, user_id, threshold in log_data:
         if user_id in bot_ids:
-            continue
+            if user_id not in user_actions:
+                user_actions[user_id] = set()
+            user_actions[user_id].add(threshold)
+    return user_actions
 
-        if user_id not in user_actions:
-            user_actions[user_id] = set()
-
-        user_actions[user_id].add(threshold)
-        power_users = []
-        
-    for user_id, actions in user_actions.items():
-        if len(actions) > threshold:
+def get_legit_power_users(log_data, bot_ids, threshold):
+    user_actions = filter_bots(log_data, bot_ids, threshold)
+    power_users = []
+    for user_id in user_actions:
+        if len(user_actions[user_id]) > threshold:
             power_users.append(user_id)
 
     return sorted(power_users)
